7bea24767944de1904cff279b4e1f7bf084b31b8
quintonn <q10athome@gmail.com>
2019-04-22 20:20:35 +0300

    change code a lot

---
+++ example-smart-app/src/js/app/patient/patient.js
@@ -0,0 +1,58 @@
+﻿(function (app)
+{
+    'use strict';
+
+    PatientController.$inject = ['$scope', 'dataService'];
+
+    function PatientController($scope, dataService)
+    {
+        var self = this;
+        self.loading = false;
+        self.ready = false;
+        self.error = "";
+
+        self.firstName = "";
+        self.lastName = "";
+        self.gender = "";
+        self.birthDate = '';
+        self.deathDate = '';
+
+        function loadData()
+        {
+            self.loading = true;
+            dataService.getPatientData().then(function (data)
+            {
+                console.log(data);
+                self.gender = data.gender;
+                self.firstName = data.name[0].given.join(' ');
+                self.lastName = data.name[0].family.join(' ');
+                self.birthDate = patient.birthDate;
+
+                self.ready = true;
+            }).catch(function (err)
+            {
+                console.log(err);
+                self.error = err;
+            }).then(function ()
+            {
+                self.loading = false;
+                $scope.$apply();
+            });
+        }
+
+        self.loading = true;
+        setTimeout(function ()
+        {
+            loadData();
+        }, 1000);
+    }
+
+    app.component('patient', {
+        templateUrl: function ()
+        {
+            return "./src/js/app/patient/patient.html";
+        },
+        controller: PatientController,
+    });
+
+}) (angular.module(constants.appName));
---
+++ example-smart-app/src/js/app/services/dataService.js
@@ -0,0 +1,94 @@
+﻿(function (app)
+{
+    'use strict';
+
+    function dataService()
+    {
+        var self = this;
+        self.smart = null;
+        self.error = "";
+        self.ready = false;
+
+        var service =
+        {
+            getPatientData: getPatientData
+        };
+
+        function getPatientData()
+        {
+            if (self.ready == false && self.error == "")
+            {
+                return new Promise(function (res, rej)
+                {
+                    setTimeout(function ()
+                    {
+                        return getPatientData().then(function (resp)
+                        {
+                            res(resp);
+                        }).catch(rej);
+                    }, 100);
+                });
+            }
+            if (self.error != "")
+            {
+                return Promise.reject(self.error);
+            }
+
+            var patient = self.smart.patient.read();
+
+            var result = $.Deferred();
+
+            $.when(patient).fail(function (e)
+            {
+                result.reject(e);
+            });
+
+            $.when(patient).done(function (patient)
+            {
+                result.resolve(patient);
+            });
+
+            return result.promise();
+        }
+
+        function onReady(smart)
+        {
+            self.smart = smart;
+            self.ready = true;
+
+            //$('#loading').hide();
+        }
+
+        function onError(e)
+        {
+            console.log(e);
+            //self.error = e;
+            self.ready = false;
+            onReady();
+        }
+        
+        try
+        {
+            /*
+            var x= FHIR.oauth2.authorize({
+                'client_id': '2335dd3f-27a5-4780-8654-b068f85afaf6',
+                'scope': 'patient/Patient.read patient/Observation.read patient/Encounter.read patient/AllergyIntolerance.read user/Patient.read user/Observation.read user/Encounter.read launch/patient launch/encounter online_access openid profile'
+            });
+            console.log('xxxxxx');
+            console.log(x);
+            */
+        }
+        catch (err)
+        {
+            console.log("ERROR");
+            console.log(err);
+        }
+
+        FHIR.oauth2.ready(onReady, onError);
+
+        return service;
+    }
+
+    app.service('dataService', dataService);
+
+})(angular.module(constants.appName));
